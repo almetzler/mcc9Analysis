400e31507a08bc9c100c0734dfeaa12be3b3006a
Alexis Metzler <metzlera@umich.edu>
2020-06-11 15:58:40 -0400

    comparing data and filtered

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -564,11 +564,12 @@ for field in dirtInfo:
   trackDirt = trackDirt.join(filteredDirt['%s' % field], on=["run", "subrun", "event"])
 
 
-print trackDirt.head(3)
-print trackExtC2.head(3)
-print trackData.head(3)
-print filteredData.head(3)
-print filteredEvents.head(3)
+# print trackDirt.head(3)
+# print trackExtC2.head(3)
+print trackData.head(10)
+print filteredData.head(10)
+#print filteredEvents.head(3)
+
 # weightsPreAverage = dirtCVWeights['wgt_tune'].to_numpy()
 # weightsPreAverageRMS = np.nanstd(weightsPreAverage)
 
