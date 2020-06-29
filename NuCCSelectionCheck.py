6147b135bd276faba5d4cbe54130246b958dc6fb
Alexis Metzler <metzlera@umich.edu>
2020-06-29 12:26:03 -0400

    added some print statements

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -398,6 +398,7 @@ def getPurity(dataframe):
 def getEfficiency(dataframe):
   numEvents = dataframe.query('isTrueCC == True').shape[0]
   totalEvents = trackOverlay.query('isTrueCC == True').shape[0]
+  print '{}: numEvents = {}, totalEvents = {}'.format(dataframe, numEvents, totalEvents)
   efficiency = float(numEvents)/float(totalEvents)
   return efficiency
 
