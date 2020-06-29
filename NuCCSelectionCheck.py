f4c03f2dde92e986429a0f135a2fd4030ffdcf40
Alexis Metzler <metzlera@umich.edu>
2020-06-29 12:06:08 -0400

    changed getEff def and commented out getPur

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -396,27 +396,27 @@ def getPurity(dataframe):
   return purity
 
 def getEfficiency(dataframe):
-  numEvents = dataframe.shape[0]
-  totalEvents = trackOverlay.shape[0]
+  numEvents = dataframe.query('isTrueCC == True').shape[0]
+  totalEvents = trackOverlay.query('isTrueCC == True').shape[0]
   efficiency = float(numEvents)/float(totalEvents)
   return efficiency
 
 def print_efficiency_and_purity():
-  print "Track Purity: {}".format(getPurity(trackOverlay))
+  # print "Track Purity: {}".format(getPurity(trackOverlay))
 
-  print "NuScore Purity: {}".format(getPurity(overlayNuScore))
+  # print "NuScore Purity: {}".format(getPurity(overlayNuScore))
   print "NuScore Efficiency: {}".format(getEfficiency(overlayNuScore))
   
-  print "TrackScore Purity: {}".format(getPurity(overlayTrackScore))
+  # print "TrackScore Purity: {}".format(getPurity(overlayTrackScore))
   print "TrackScore Efficiency: {}".format(getEfficiency(overlayTrackScore))
 
-  print "PIDScore Purity: {}".format(getPurity(overlayPIDScore))
+  # print "PIDScore Purity: {}".format(getPurity(overlayPIDScore))
   print "PIDScore Efficiency: {}".format(getEfficiency(overlayPIDScore))
 
-  print "MuonCandidate Purity: {}".format(getPurity(overlayMuonCandidates))
+  # print "MuonCandidate Purity: {}".format(getPurity(overlayMuonCandidates))
   print "MuonCandidate Efficiency: {}".format(getEfficiency(overlayMuonCandidates))
   
-  print "InclusiveEvents Purity: {}".format(getPurity(overlayInclusiveEvents))
+  # print "InclusiveEvents Purity: {}".format(getPurity(overlayInclusiveEvents))
   print "InclusiveEvents Efficiency: {}".format(getEfficiency(overlayInclusiveEvents))
 
 InputFiles = ["/uboone/data/users/joelam/stv-ntuples-new/numu_run1.root", "/uboone/data/users/joelam/stv-ntuples-new/bnb_5e19_run1.root", "/uboone/data/users/joelam/stv-ntuples-new/extC1_run1.root", "/uboone/data/users/joelam/stv-ntuples-new/dirt_run1.root", "/uboone/data/users/joelam/stv-ntuples-new/extC2_run1.root"]
