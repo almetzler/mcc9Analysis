408a3ecaae401bf546c971ed87692d019aacf190
Alexis Metzler <metzlera@umich.edu>
2020-07-07 09:39:10 -0400

    forgot to define props

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -204,6 +204,8 @@ def makeDataMCRatioHistogram(mcList, mcWeights, dataList, binRange, nBins, filen
   xmax = ax.get_xlim()[1]
   #print "Min %.2f Max %.2f" % (ax.get_xlim()[0], ax.get_xlim()[1])
   # plt.text(0.7*xmax, 0.9*ymax, text, {'fontsize' : 18})
+  props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+  
   axi.text(0.75, 1.1, text, transform=axi.transAxes, fontsize=14,
         verticalalignment='top', bbox=props)
   plt.savefig("%s/%sRatio.png" % ( dir_name, filename) )
