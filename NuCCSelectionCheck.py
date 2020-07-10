a44faae874592c58da59f338b9f2b4644b05bfc7
Alexis Metzler <metzlera@umich.edu>
2020-07-10 12:17:20 -0400

    had an extra parenthesis

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -738,7 +738,7 @@ makeDataMCHistogram(incTrkScoreStack, incTrkScorekWeights, dataNuScore['track_sc
 
 nuSliceScoreStack = Stack(overlayNuScore, dirtNuScore, extNuScore, 'nu_score')
 
-makeDataMCHistogram(nuSliceScoreStack, incTrkScorekWeights, dataNuScore['nu_score'].to_numpy(),trkScoreRange, 25, "NuSliceScore", ["Slice Score", "Score", "Number of Daughters"]))
+makeDataMCHistogram(nuSliceScoreStack, incTrkScorekWeights, dataNuScore['nu_score'].to_numpy(),trkScoreRange, 25, "NuSliceScore", ["Slice Score", "Score", "Number of Daughters"])
 
 extTrackScore     = trackExt.query('DuplicatedEvent == False & track_score > @minTrackScore')
 dirtTrackScore    = trackDirt.query('DuplicatedEvent == False & track_score > @minTrackScore')
