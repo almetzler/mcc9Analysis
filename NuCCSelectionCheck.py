a4bd7c13bf5e42c55440f8981df59a21da90c42b
Alexis Metzler <metzlera@umich.edu>
2020-07-14 13:10:26 -0400

    removed a cut

--- NuCCSelectionCheck.py
+++ NuCCSelectionCheck.py
@@ -905,13 +905,13 @@ overlayInclusiveEvents_noChi2 = overlayMuonCandidates.query('isLongestTrack == T
 dataInclusiveEvents_noChi2 = dataMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & flash_chi2_ratio < @maxFlashChi2Ratio & nu_score > @minNeutrinoScore & nu_score > @minNeutrinoScoreFlashFails')
 
 
-extInclusiveEvents_noChi2Ratio = extMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore & nu_score > @minNeutrinoScoreFlashFails')
+extInclusiveEvents_noChi2Ratio = extMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore')
 
-dirtInclusiveEvents_noChi2Ratio = dirtMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore & nu_score > @minNeutrinoScoreFlashFails')
+dirtInclusiveEvents_noChi2Ratio = dirtMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore')
 
-overlayInclusiveEvents_noChi2Ratio = overlayMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore & nu_score > @minNeutrinoScoreFlashFails')
+overlayInclusiveEvents_noChi2Ratio = overlayMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore')
 
-dataInclusiveEvents_noChi2Ratio = dataMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore & nu_score > @minNeutrinoScoreFlashFails')
+dataInclusiveEvents_noChi2Ratio = dataMuonCandidates.query('isLongestTrack == True & isFiducial == True & nu_pdg == @numupdg & daughters_start_contained == True & nu_score > @minNeutrinoScore')
 # #SAVE THIS
 # #print dataInclusiveEvnets.loc[(5774, 15,762 ),('track_chi2_muon', 'track_chi2_proton', 'track_chi2_ratio', 'isFiducial', 'nu_score', 'nu_flash_chi2', 'nu_mu_cc_selected')]
 
@@ -995,8 +995,8 @@ chi2_phi = Stack(overlayInclusiveEvents_noChi2Ratio, dirtInclusiveEvents_noChi2R
 chi2_nu = Stack(overlayInclusiveEvents_noChi2Ratio, dirtInclusiveEvents_noChi2Ratio, extInclusiveEvents_noChi2Ratio, 'nu_score', True)
 chi2_wgt = Stack(overlayInclusiveEvents_noChi2Ratio, dirtInclusiveEvents_noChi2Ratio, extInclusiveEvents_noChi2Ratio, 'wgt', True)
 
-makeDataMCHistogram(chi2_phi, chi2_wgt, dataInclusiveEvents_noChi2Ratio['phi'].to_numpy(), phiRange, 64, "phi_1-5", ["phi cuts 1-5", "Angle / pi (radians)", "Number of Primary Muons"])
-makeDataMCHistogram(chi2_nu, chi2_wgt, dataInclusiveEvents_noChi2Ratio['nu_score'].to_numpy(), trkScoreRange, 50, "nu_score_1-5", ["nu_score cuts 1-5", "Neutrino ID", "Number of Events"])
+makeDataMCHistogram(chi2_phi, chi2_wgt, dataInclusiveEvents_noChi2Ratio['phi'].to_numpy(), phiRange, 64, "phi_1-4", ["phi cuts 1-4", "Angle / pi (radians)", "Number of Primary Muons"])
+makeDataMCHistogram(chi2_nu, chi2_wgt, dataInclusiveEvents_noChi2Ratio['nu_score'].to_numpy(), trkScoreRange, 50, "nu_score_1-4", ["nu_score cuts 1-4", "Neutrino ID", "Number of Events"])
 
 '''
 df_list = [(trackOverlay, trackDirt, trackExt),(overlayNuScore, dirtNuScore, extNuScore),(overlayTrackScore, dirtTrackScore, extTrackScore),(overlayPIDScore, dirtPIDScore, extPIDScore),(overlayMuonCandidates, dirtMuonCandidates, extMuonCandidates),(overlayInclusiveEvents, dirtInclusiveEvents, extInclusiveEvents),(overlayInclusiveEvents_noChi2, dirtInclusiveEvents_noChi2, extInclusiveEvents_noChi2)]
