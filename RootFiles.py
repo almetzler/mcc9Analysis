9df05397165f4a489d3cee7c6cd553120cf73ad7
Alexis Metzler <metzlera@umich.edu>
2020-11-13 14:47:51 -0500

    im just trying random things at this point :/

--- RootFiles.py
+++ RootFiles.py
@@ -80,25 +80,25 @@ with open(filepath, 'w') as f:
 
 '''        
 
-eventsExtJSON = eventsExt.to_json(orient = 'index')
+eventsExtJSON = eventsExt.to_json(orient = 'split')
 with open("Data/eventsExt",'w') as fle:
     for chunk in json.JSONEncoder().iterencode(eventsExtJSON):
         fle.write(json.dumps(chunk))
 print('eventExt done')
 
-eventsOnBeamJSON = eventsOnBeam.to_json(orient='index')
+eventsOnBeamJSON = eventsOnBeam.to_json(orient='split')
 with open("Data/eventsOnBeam",'w') as fle: 
     for chunk in json.JSONEncoder().iterencode(eventsOnBeamJSON):
         fle.write(json.dumps(chunk))
 print('eventsOnBeam done')
 
-eventsOverlayJSON = eventsOverlay.to_json(orient='index')
+eventsOverlayJSON = eventsOverlay.to_json(orient='split')
 with open("Data/eventsOverlay",'w') as fle:
     for chunk in json.JSONEncoder().iterencode(eventsOverlayJSON):
         fle.write(json.dumps(chunk))
 print('eventsOverlay done')
 
-eventsDirtJSON = eventsDirt.to_json(orient='index')
+eventsDirtJSON = eventsDirt.to_json(orient='split')
 with open("Data/eventsDirt",'w') as fle:
     for chunk in json.JSONEncoder().iterencode(eventsDirtJSON):
         fle.write(json.dumps(chunk))
